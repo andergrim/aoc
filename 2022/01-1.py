7a66c3442c0c5848c1e56e5ba6edef963aca6324
Kristoffer Andergrim <kristoffer.andergrim@wirelesscar.com>
2022-12-01 16:20:39 +0100

    day 1

---
+++ 2022/01-1.py
@@ -0,0 +1,12 @@
+elves = []
+
+with open('01.txt') as fp:
+    stash = []
+    for line in fp:
+        if line.strip().isnumeric():
+            stash.append(int(line.strip()))
+        else:
+            elves.append(sum(stash))
+            stash = []
+
+print(sorted(elves, reverse=True)[0])
