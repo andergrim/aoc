9075a6cd4c0a49bbf86a8810b6aa29e0cf71746c
Kristoffer Andergrim <kristoffer.andergrim@wirelesscar.com>
2022-12-04 18:13:00 +0100

    Day 4

---
+++ 2022/04-1.py
@@ -0,0 +1,12 @@
+overlaps = 0
+
+with open('04.txt') as fp:
+    for pair in fp:
+        section_ranges = [n.split("-") for n in pair.strip().split(",")]
+        first_range = {n for n in range(int(section_ranges[0][0]), int(section_ranges[0][1])+1)}
+        second_range = {n for n in range(int(section_ranges[1][0]), int(section_ranges[1][1])+1)}
+
+        if first_range.issubset(second_range) or second_range.issubset(first_range):
+            overlaps += 1
+
+print(overlaps)
